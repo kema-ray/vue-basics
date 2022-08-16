9d305c2b2f59414e9a2089a03a0d758f882d9960
rachel <rachelkemuma99@gmail.com>
2022-08-16 17:37:18 +0300

    add a function and return data in it

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finish the course and learn vue'
+        }
+    }
+});
 
 app.mount('#user-goal');
