1119f83eb21c0a911cd651d902738d67ff72868a
rachel <rachelkemuma99@gmail.com>
2022-08-17 17:14:50 +0300

    add a button that adds and removes events

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
     data() {
-        return {
-            courseGoalA: 'Finish the course and learn vue!',
-            courseGoalB: 'Master Vue and build awesome apps',
-            // courseGoalB: '<h2>Master Vue and build awesome apps</h2>',
-            vueLink: 'https://vuejs.org/'
-        };
+      return {
+        counter: 0,
+      };
     },
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
-        }
-    }
-});
-
-app.mount('#user-goal');
+  });
+  
+  app.mount('#events');
